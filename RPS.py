95a20281b75652cf524bbf0b342988ad07628bd2
rileyjmayfield <rileyjmayfield@gmail.com>
2026-02-04 04:26:31 +0000

    Temp and RPS complete

--- RPS.py
+++ RPS.py
@@ -1,22 +1,84 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name:Riley Mayfield
+#Date:2/3
+#Assignment:Lab 2
+#Purpose: Make a rock paper scissors game
 import random
 
 def main():
   wins = 0
   ties = 0
   losses = 0
+  play = "yes"
+  hands = ["R","P","S"]
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
 
+  play = input("shall we play a game?(yes/no): ")
+  while play != "yes" and play != "no":
+    print("I don't understand")
+    play = input("shall we play a game?(yes/no): ")
+
+
+  while play == "yes":
+    compPlay = random.choice(hands)
+    #print(compPlay)
+    mePlay = input("I made my choice, what's yours?(R/P/S): ")
+    print("I chose: ",compPlay,"\nYou chose: ",mePlay)
+
+    if compPlay == "R":
+      if mePlay == "R":
+        ties = ties + 1
+        print("tie")
+      elif mePlay == "P":
+        wins = wins + 1
+        print("you win")
+      elif mePlay == "S":
+        losses = losses + 1
+        print("you lose")
+      else:
+        print("Not a valid hand")
+
+    if compPlay == "P":
+      if mePlay == "P":
+        ties = ties + 1
+        print("tie")
+      elif mePlay == "S":
+        wins = wins + 1
+        print("you win")
+      elif mePlay == "R":
+        losses = losses + 1
+        print("you lose")
+      else:
+        print("Not a valid hand")
+
+    if compPlay == "S":
+      if mePlay == "S":
+        ties = ties + 1
+        print("tie")
+      elif mePlay == "R":
+        wins = wins + 1
+        print("you win")
+      elif mePlay == "P":
+        losses = losses + 1
+        print("you lose")
+      else:
+        print("Not a valid hand")
+    
+    play = input("shall we play a game?(yes/no): ")
+    while play != "yes" and play != "no":
+      print("I don't understand")
+      play = input("shall we play a game?(yes/no): ")
+    
+
+  
   #Randomly choose the computer between 'R', 'P', or 'S'
   #Prompt the user for their RPS selection
   #Determine winner and state what happened to the user
   #Ask the user if they would like to play again.
 
   #In the end, print the stats
+  print("Goodbye")
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
   print(wins, "\t", ties , "\t", losses)
--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,17 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
-
+#Name:Riley Mayfield
+#Date:2/3
+#Assignment:Lab 3
+#Purpose: convert F to C
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  tempF = input("What's the tempurature in Fahrenheit? :")
+  tempF = int(tempF)
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
-
-  tempC = tempF / 2
-
-  print(tempF, "is ", tempC, "degrees celsius.")
+  tempC = (tempF-32)*5/9
+  tempC = round(tempC,1)
+  print(tempF, "degrees Fahrenheit is ", tempC, "degrees Celsius.")
 if __name__ == '__main__':
   main()
